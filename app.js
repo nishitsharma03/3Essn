a964f56520b050cd5eee60108ad3e3e5305473d0
shubhammehra4 <shubhammehra4@gmail.com>
2020-09-08 22:41:10 +0530

    app config

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.get("/register", function (req, res) {
     res.render("register");
 })
 
+app.post("/register", function (req, res) {
+    res.send("Registration request recieved");
+})
+
 app.get("/login", function (req, res) {
     res.render("login");
 });
