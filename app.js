ed80dfac382021ff6c5d1c7b4476abceaf652608
shubhammehra4 <shubhammehra4@gmail.com>
2020-09-09 23:53:36 +0530

    minor adjustements added user auth

--- app.js
+++ app.js
@@ -2,20 +2,57 @@ var express           = require("express"),
 bodyParser            = require("body-parser"),
 mongoose              = require("mongoose"),
 passport              = require("passport"),
-localStrategy         = require("passport-local"),
-passportLocalMongoose = require("passport-local-mongoose");
+LocalStrategy         = require("passport-local"),
+passportLocalMongoose = require("passport-local-mongoose"),
+User                  = require("./models/user"),
+seedDB                = require("./seeds"),
+app                   = express();
 
+// ============
+// APP CONGIG
+// ============
 
-var app = express();
-// mongoose.connect("mongodb://127.0.0.1:27017/btpptoj_2020");
+mongoose.connect("mongodb://127.0.0.1:27017/btpproj_2020");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+seedDB();
 
+app.use(require("express-session")({
+    secret: "LKLKLK HVGYCU Ghuvggu bhjguhu",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+})
+
+// =============
+// Basic ROUTES
+// =============
 
 app.get("/", function (req, res) {
     res.render("main/index");
 });
+
+app.get("/resources", function (req, res) {
+    res.send("Resources");
+})
+
+app.get("/aboutus", function (req, res) {
+    res.send("About Us!!");
+})
+
+// ============
+// AUTH ROUTES
+// ============
 
 app.get("/user", function (req, res) {
     res.send("this is the users page");
@@ -23,23 +60,55 @@ app.get("/user", function (req, res) {
 
 app.get("/register", function (req, res) {
     res.render("register/form");
-})
+});
 
 app.post("/register", function (req, res) {
-    res.send("Registration request recieved");
-})
+    var newUser = new User({
+        firstName: req.body.firstName,
+        lastName : req.body.lastName,
+        username : req.body.username,
+        email    : req.body.email,
+        phoneNo  : req.body.phoneNo,
+    });
+    User.register(newUser, req.body.password, function (err, user) {
+        if(err){
+            console.log(err);
+            return res.render("register/form");
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/");
+        });
+    });
+});
 
 app.get("/login", function (req, res) {
     res.render("login/form");
 });
 
-app.post("/login", function (req, res) {
-   res.send("Login request recieved will add processing after sometime");     
+app.post("/login",passport.authenticate("local",
+    {
+        successRedirect: "/",
+        failureRedirect: "/login"
+    }), function (req, res) {
 });
+
+app.get("/logout", function (req, res) {
+    req.logOut();
+    res.redirect("/");
+});
+
+// DEFAULT ROUTE
 
 app.get("*", function (req, res) {
     res.send("Webpage does not exist!!");
 });
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen("3000", function () {
     console.log("Server is running!");
--- models/user.js
+++ models/user.js
@@ -1,10 +1,16 @@
-var mongoose = require("mongoose");
+var mongoose          = require("mongoose"),
+passportLocalMongoose = require("passport-local-mongoose");
+
 
  var UserSchema = new mongoose.Schema({
      firstName: String,
      lastName : String,
+     username : String,
      email    : String,
      phoneNo  : String,
-     username : String,
      password : String
  });
+
+UserSchema.plugin(passportLocalMongoose);
+
+ module.exports = mongoose.model("User", UserSchema);
