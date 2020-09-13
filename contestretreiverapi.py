3606d10e97214dbc0401c1dd524d11a9309b140e
Nishit Sharma <nishitsharma0@gmail.com>
2020-09-13 19:46:38 +0530

    updated api to retrieve only ongoing and future contests

--- contestretreiverapi.py
+++ contestretreiverapi.py
@@ -1,8 +1,12 @@
 import requests
 import json
+from datetime import date
 
+today=date.today()
+#print(str(today))
+#print("https://clist.by:443/api/v1/contest/?start__gte="+ str(today) +"T00%3A00%3A00&order_by=start&username=nishitsharm03&api_key=34bb37ac0dc7d26189a9b939cb2a5ab55ae584a0")
 
-result= requests.get("https://clist.by:443/api/v1/contest/?username=nishitsharm03&api_key=34bb37ac0dc7d26189a9b939cb2a5ab55ae584a0")
+result= requests.get("https://clist.by:443/api/v1/contest/?start__gte=" + str(today) +"T00%3A00%3A00&order_by=start&username=nishitsharm03&api_key=34bb37ac0dc7d26189a9b939cb2a5ab55ae584a0")
 
 if result.status_code!=200:
     print('Unable to get data')
@@ -10,10 +14,7 @@ if result.status_code!=200:
 
 res=json.loads(result.content.decode('utf-8'));
 # print(type(result))
-with open('data.json','w') as outfile:
 
-    # for i in res:
-    json.dump(res,outfile)
 
 
 
@@ -25,12 +26,11 @@ finallist={}
 cnt=0
 for i in contests:
     if i['resource']['name'] in names:
-        print(i, type(i))
+        #print(i, type(i))
         finallist[cnt]=i
         cnt+=1;
 
-for i in finallist:
-    print(i)
+
 with open('data.json','w') as outfile:
 
     # for i in res:
